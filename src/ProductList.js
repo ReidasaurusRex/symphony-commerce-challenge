fef0d4ba72eba538e8da0c3fd494ac55612ea5bf
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-09 19:11:18 -0800

    Added constructor function for ProductList

--- src/ProductList.js
+++ src/ProductList.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import Product from './Product';
 
 class ProductList extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      products: []
+    }
+  }
 }
 
 export default ProductList;
