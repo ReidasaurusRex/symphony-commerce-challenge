a352668ed13ccbb5a42d192bef50a3026d842654
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-09 19:32:09 -0800

    Added classNames to app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import ProductList from './ProductList'
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Extras</h1>
-        <p>Purchase FIJI Water Accessories ranging from Signature Silver Sleeves to FIJI Water Car Cup Holders. Accessorize your FIJI Water today!</p>
+      <div className="c-app">
+        <h1 className="c-app-title">Extras</h1>
+        <p className="c-app-description">Purchase FIJI Water Accessories ranging from Signature Silver Sleeves to FIJI Water Car Cup Holders. Accessorize your FIJI Water today!</p>
         <ProductList />
       </div>
     );
