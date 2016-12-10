b059f30d5c6b215274c8ea19234097c08aed00fa
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-09 19:16:42 -0800

    Added componentDidMount function to make ajax call to products

--- src/ProductList.js
+++ src/ProductList.js
@@ -6,13 +6,27 @@ class ProductList extends Component {
   constructor(props) {
     super(props);
 
+    this.componentDidMount = this.componentDidMount.bind(this);
+
     this.state = {
-      products: []
+      products: [],
+      loading: true;
     }
   }
 
   componentDidMount() {
-
+    var url = "https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js"
+    $.ajax({
+      url: url,
+      type: 'GET',
+      dataType: 'JSON',
+      success: function(data) {
+        this.setState({products: data.products, loading: false})
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(url, status, err.toString());
+      }.bind(this)
+    })
   }
 }
 
