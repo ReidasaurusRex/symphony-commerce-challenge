49cb3b373ec3985c0a7e5b58c9766461a5d02140
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-09 19:08:42 -0800

    Added constructor and render functions to Product component. Ran tests, creating Product snapshot

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
-
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="c-product">
+        <h2 className="c-product__name">{this.props.name}</h2>
+        <p className="c-product__price">{this.props.price}</p>
+        <img src={this.props.image} alt={this.props.name} className="c-product__image"/>
+      </div>
+    );
+  }
 }
 
 export default Product;
