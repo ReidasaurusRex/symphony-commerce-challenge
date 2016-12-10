6970e45a37f466c85b414abd526eaaa8b928c71b
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-09 19:52:56 -0800

    Added proper BEM conventions to 'c-app__title' and 'c-app__description'

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="c-app">
-        <h1 className="c-app-title">Extras</h1>
-        <p className="c-app-description">Purchase FIJI Water Accessories ranging from Signature Silver Sleeves to FIJI Water Car Cup Holders. Accessorize your FIJI Water today!</p>
+        <h1 className="c-app__title">Extras</h1>
+        <p className="c-app__description">Purchase FIJI Water Accessories ranging from Signature Silver Sleeves to FIJI Water Car Cup Holders. Accessorize your FIJI Water today!</p>
         <ProductList />
       </div>
     );
